d473dfd83ea48cb1795dc071b0ddb0eb8ffc27bd
«GGlebux» <rejngleb@gmail.com>
2024-05-16 00:26:31 +0400

    New abilities *Program can fill 3 view word`s file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # подключаем графическую библиотеку для создания интерфейсов
 from tkinter import *
 from docxtpl import DocxTemplate
+from tkinter import messagebox
 
 # подключаем графическую библиотеку
 window = Tk()
@@ -10,14 +11,15 @@ window.title("Мой бухгалтер")
 
 # обрабатываем закрытие окна
 def on_closing():
-    '''# показываем диалоговое окно с кнопкой
+    # показываем диалоговое окно с кнопкой
     if messagebox.askokcancel("", "Закрыть программу?"):
-        # удаляем окно и освобождаем память'''
-    window.destroy()
+        # удаляем окно и освобождаем память
+        window.destroy()
 
 
 # сообщаем системе о том, что делать, когда окно закрывается
 window.protocol("WM_DELETE_WINDOW", on_closing)
+
 
 '''Cоздание надписи для полей ввода и размещение их по сетке'''
 
@@ -82,94 +84,71 @@ e3 = Entry(window, textvariable=patronymic)
 e3.grid(row=3, column=1)
 
 date_birthday = StringVar()
-e3 = Entry(window, textvariable=date_birthday)
-e3.grid(row=4, column=1)
+e4 = Entry(window, textvariable=date_birthday)
+e4.grid(row=4, column=1)
 
 snils = StringVar()
-e3 = Entry(window, textvariable=snils)
-e3.grid(row=5, column=1)
+e5 = Entry(window, textvariable=snils)
+e5.grid(row=5, column=1)
 
 inn = StringVar()
-e3 = Entry(window, textvariable=inn)
-e3.grid(row=6, column=1)
+e6 = Entry(window, textvariable=inn)
+e6.grid(row=6, column=1)
 
 citizenship = StringVar()
-e3 = Entry(window, textvariable=citizenship)
-e3.grid(row=7, column=1)
+e7 = Entry(window, textvariable=citizenship)
+e7.grid(row=7, column=1)
 
 id_doc = StringVar()
-e3 = Entry(window, textvariable=id_doc)
-e3.grid(row=8, column=1)
+e8 = Entry(window, textvariable=id_doc)
+e8.grid(row=8, column=1)
 
 series = StringVar()
-e3 = Entry(window, textvariable=series)
-e3.grid(row=9, column=1)
+e9 = Entry(window, textvariable=series)
+e9.grid(row=9, column=1)
 
 number = StringVar()
-e3 = Entry(window, textvariable=number)
-e3.grid(row=11, column=1)
+e10 = Entry(window, textvariable=number)
+e10.grid(row=11, column=1)
 
 date_id_doc = StringVar()
-e3 = Entry(window, textvariable=date_id_doc)
-e3.grid(row=12, column=1)
+e11 = Entry(window, textvariable=date_id_doc)
+e11.grid(row=12, column=1)
 
 office_doc = StringVar()
-e3 = Entry(window, textvariable=office_doc, width=45)
-e3.grid(row=12, column=2)
+e12 = Entry(window, textvariable=office_doc, width=45)
+e12.grid(row=12, column=2)
 
 address = StringVar()
-e3 = Entry(window, textvariable=address, width=45)
-e3.grid(row=13, column=1)
+e13 = Entry(window, textvariable=address, width=45)
+e13.grid(row=13, column=1)
 
 tel_number = StringVar()
-e3 = Entry(window, textvariable=tel_number, width=45)
-e3.grid(row=14, column=1)
+e14 = Entry(window, textvariable=tel_number, width=45)
+e14.grid(row=14, column=1)
 
+profession = 'Профессия'
+specialty = 'Специальность'
+specialty_with_exam = 'Специальность c экзаменом'
 
-def rad_but_show():
-    print(f'Выбраны профессии {NONE}')
+choice = StringVar(value=profession)
 
-
-'''rb_status = 0
-rb = Radiobutton(window, text='Профессии', variable=rb_status, command=rad_but_show(), state=NORMAL)
+rb = Radiobutton(window, text=profession, value=profession, variable=choice)
 rb.grid(row=3, column=3)
 
-rb1 = Radiobutton(window, text='Специальности', variable=rb_status, command=rad_but_show(), state=NORMAL)
+rb1 = Radiobutton(window, text=specialty, value=specialty, variable=choice)
 rb1.grid(row=4, column=3)
 
-rb2 = Radiobutton(window, text='Специальности с экз', variable=rb_status, command=rad_but_show(), state=NORMAL)
+rb2 = Radiobutton(window, text=specialty_with_exam, value=specialty_with_exam, variable=choice)
 rb2.grid(row=5, column=3)
-'''
 
 
-def show_message():
-    print(date_id_doc.get())
 
-
-# создаём кнопки действий и привязываем их к своим функциям
-# кнопки размещаем тоже по сетке
-'''b1 = Button(window, text="Посмотреть все", width=12)
-b1.grid(row=2, column=3)  # size of the button
-
-b2 = Button(window, text="Поиск", width=12)
-b2.grid(row=3, column=3)
-
-b3 = Button(window, text="Добавить", width=12)
-b3.grid(row=4, column=3)
-
-b4 = Button(window, text="Обновить", width=12)
-b4.grid(row=5, column=3)
-
-b5 = Button(window, text="Удалить", width=12)
-b5.grid(row=6, column=3)
-
-b6 = Button(window, text="Закрыть", width=12, command=on_closing)
-b6.grid(row=7, column=3)'''
 
 
 def fill():
     '''Заполнить документ Word информацией введенной из интерфейса'''
-    doc = DocxTemplate('prof.docx')
+
     context = {'reg_number': reg_number.get(),
                'surname': surname.get(),
                'name': name.get(),
@@ -181,15 +160,22 @@ def fill():
                'id_doc': id_doc.get(),
                'series': series.get(),
                'number': number.get(),
-               'day_doc': date_id_doc.get().split('.')[0],
-               'month_doc': date_id_doc.get().split('.')[1],
-               'year_doc': date_id_doc.get().split('.')[1][-2:],
+               'day_doc': date_id_doc.get(),
+               'month_doc': date_id_doc.get(),
+               'year_doc': date_id_doc.get(),
                'office_doc': office_doc.get(),
                'address': address.get(),
                'tel_number': tel_number.get()
                }
+    if choice == profession:
+        doc = DocxTemplate('patterns/prof.docx')
+    elif choice == specialty:
+        doc = DocxTemplate('patterns/special.docx')
+    else:
+        doc = DocxTemplate('patterns/sp_with_ex.docx')
     doc.render(context)
-    doc.save("res.docx")
+    doc.save(f'{surname.get()} {name.get()} {patronymic.get()}.docx')
+    '''    '''
 
 
 btn = Button(window, text="Заполнить", width=12, command=fill)
