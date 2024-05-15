a956b48b10fae941791860b31d701179b47a2565
«GGlebux» <rejngleb@gmail.com>
2024-05-16 01:57:34 +0400

    New abilities *Can choice path of file save

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from tkinter import *
 from docxtpl import DocxTemplate
 from tkinter import messagebox
+from tkinter import filedialog
+
+SAVE_DIRECTORY = ''
+FILE_NAME = ''
 
 # подключаем графическую библиотеку
 window = Tk()
@@ -20,50 +24,52 @@ def on_closing():
 # сообщаем системе о том, что делать, когда окно закрывается
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
-
 '''Cоздание надписи для полей ввода и размещение их по сетке'''
 
-l1 = Label(window, text="Регистрационный номер")
-l1.grid(row=0, column=0)
+l1 = Label(window, text='Регистрационный номер')
+l1.grid(row=0, column=0, sticky=E)
 
 l2 = Label(window, text='Фамилия')
-l2.grid(row=1, column=0)
+l2.grid(row=1, column=0, sticky=E)
 
 l3 = Label(window, text='Имя')
-l3.grid(row=2, column=0)
+l3.grid(row=2, column=0, sticky=E)
 
 l4 = Label(window, text='Отчество')
-l4.grid(row=3, column=0)
+l4.grid(row=3, column=0, sticky=E)
 
 l5 = Label(window, text='Дата рождения')
-l5.grid(row=4, column=0)
+l5.grid(row=4, column=0, sticky=E)
 
 l6 = Label(window, text='СНИЛС')
-l6.grid(row=5, column=0)
+l6.grid(row=5, column=0, sticky=E)
 
 l7 = Label(window, text='ИНН')
-l7.grid(row=6, column=0)
+l7.grid(row=6, column=0, sticky=E)
 
 l8 = Label(window, text='Гражданство')
-l8.grid(row=7, column=0)
+l8.grid(row=7, column=0, sticky=E)
 
 l9 = Label(window, text='Документ, удостоверяющий личность')
-l9.grid(row=8, column=0)
+l9.grid(row=8, column=0, sticky=E)
 
 l10 = Label(window, text='Серия')
-l10.grid(row=9, column=0)
+l10.grid(row=9, column=0, sticky=E)
 
 l12 = Label(window, text='Номер')
-l12.grid(row=11, column=0)
+l12.grid(row=11, column=0, sticky=E)
 
 l13 = Label(window, text='Когда и кем выдан')
-l13.grid(row=12, column=0)
+l13.grid(row=12, column=0, sticky=E)
 
 l14 = Label(window, text='Проживающего(ей) по адресу')
-l14.grid(row=13, column=0)
+l14.grid(row=13, column=0, sticky=E)
 
 l15 = Label(window, text='Телефон')
-l15.grid(row=14, column=0)
+l15.grid(row=14, column=0, sticky=E)
+
+l16 = Label(window, text=f'Путь сохранения {SAVE_DIRECTORY}', width=40)
+l16.grid(row=15, column=0, sticky=E)
 
 '''Создание полей для ввода данных'''
 
@@ -72,59 +78,59 @@ e1 = Entry(window, textvariable=reg_number, width=30)
 e1.grid(row=0, column=1)
 
 surname = StringVar()
-e2 = Entry(window, textvariable=surname)
+e2 = Entry(window, textvariable=surname, width=30)
 e2.grid(row=1, column=1)
 
 name = StringVar()
-e3 = Entry(window, textvariable=name)
+e3 = Entry(window, textvariable=name, width=30)
 e3.grid(row=2, column=1)
 
 patronymic = StringVar()
-e3 = Entry(window, textvariable=patronymic)
+e3 = Entry(window, textvariable=patronymic, width=30)
 e3.grid(row=3, column=1)
 
 date_birthday = StringVar()
-e4 = Entry(window, textvariable=date_birthday)
+e4 = Entry(window, textvariable=date_birthday, width=30)
 e4.grid(row=4, column=1)
 
 snils = StringVar()
-e5 = Entry(window, textvariable=snils)
+e5 = Entry(window, textvariable=snils, width=30)
 e5.grid(row=5, column=1)
 
 inn = StringVar()
-e6 = Entry(window, textvariable=inn)
+e6 = Entry(window, textvariable=inn, width=30)
 e6.grid(row=6, column=1)
 
 citizenship = StringVar()
-e7 = Entry(window, textvariable=citizenship)
+e7 = Entry(window, textvariable=citizenship, width=30)
 e7.grid(row=7, column=1)
 
 id_doc = StringVar()
-e8 = Entry(window, textvariable=id_doc)
+e8 = Entry(window, textvariable=id_doc, width=30)
 e8.grid(row=8, column=1)
 
 series = StringVar()
-e9 = Entry(window, textvariable=series)
+e9 = Entry(window, textvariable=series, width=30)
 e9.grid(row=9, column=1)
 
 number = StringVar()
-e10 = Entry(window, textvariable=number)
+e10 = Entry(window, textvariable=number, width=30)
 e10.grid(row=11, column=1)
 
 date_id_doc = StringVar()
-e11 = Entry(window, textvariable=date_id_doc)
-e11.grid(row=12, column=1)
+e11 = Entry(window, textvariable=date_id_doc, width=25)
+e11.grid(row=12, column=1, sticky=W)
 
 office_doc = StringVar()
-e12 = Entry(window, textvariable=office_doc, width=45)
+e12 = Entry(window, textvariable=office_doc, width=30)
 e12.grid(row=12, column=2)
 
 address = StringVar()
-e13 = Entry(window, textvariable=address, width=45)
+e13 = Entry(window, textvariable=address, width=30)
 e13.grid(row=13, column=1)
 
 tel_number = StringVar()
-e14 = Entry(window, textvariable=tel_number, width=45)
+e14 = Entry(window, textvariable=tel_number, width=30)
 e14.grid(row=14, column=1)
 
 profession = 'Профессия'
@@ -141,9 +147,6 @@ rb1.grid(row=4, column=3)
 
 rb2 = Radiobutton(window, text=specialty_with_exam, value=specialty_with_exam, variable=choice)
 rb2.grid(row=5, column=3)
-
-
-
 
 
 def fill():
@@ -167,19 +170,43 @@ def fill():
                'address': address.get(),
                'tel_number': tel_number.get()
                }
+    # Проверка вида заполнения документа
     if choice == profession:
         doc = DocxTemplate('patterns/prof.docx')
     elif choice == specialty:
         doc = DocxTemplate('patterns/special.docx')
     else:
         doc = DocxTemplate('patterns/sp_with_ex.docx')
+
     doc.render(context)
-    doc.save(f'{surname.get()} {name.get()} {patronymic.get()}.docx')
-    '''    '''
+
+    file_name = surname.get() + name.get() + patronymic.get()
+
+    doc.save(save_file(file_name))
 
 
-btn = Button(window, text="Заполнить", width=12, command=fill)
-btn.grid(row=7, column=7)
+def save_file(file_name):
+    global SAVE_DIRECTORY
+
+    if not file_name:
+        file_name = 'form'
+
+    if not SAVE_DIRECTORY:
+        SAVE_DIRECTORY = filedialog.askdirectory()
+
+    return f'{SAVE_DIRECTORY}/{file_name}.docx'
+
+
+def choose_save_path():
+    global SAVE_DIRECTORY
+    SAVE_DIRECTORY = filedialog.askdirectory()
+
+
+btn1 = Button(window, text="Заполнить", width=12, command=fill)
+btn1.grid(row=13, column=3)
+
+btn2 = Button(window, text='Выбрать путь', width=12, command=choose_save_path)
+btn2.grid(row=12, column=3)
 
 # пусть окно работает всё время до закрытия
 window.mainloop()
